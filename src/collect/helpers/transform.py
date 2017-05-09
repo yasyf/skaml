df1112b72db8d82bd718bd3b6a6738c80d51b82c
Yasyf Mohamedali <yasyfm@gmail.com>
2017-05-09 19:57:42 -0400

    Filter out invalid data before transforming.

--- src/collect/helpers/transform.py
+++ src/collect/helpers/transform.py
@@ -9,14 +9,18 @@ def one_hot(char):
   one_hot[char['keyCode'] - FIRST_CODE] = 1
   return one_hot
 
+def is_valid_char(char):
+  return FIRST_CODE <= char['keyCode'] <= LAST_CODE
+
 def release_time_diff(c1, c2):
   return c2['timeReleased'] - c1['timeReleased']
 
 def transform(obj):
-  X = np.empty(len(obj['words']), dtype=np.object)
-  Y = np.empty(len(obj['words']), dtype=np.object)
-  for i, word in enumerate(obj['words']):
-    chars = word['characters']
+  words = [x for x in obj['words'] if x['characters']]
+  X = np.empty(len(words), dtype=np.object)
+  Y = np.empty(len(words), dtype=np.object)
+  for i, word in enumerate(words):
+    chars = [x for x in word['characters'] if is_valid_char(x)]
     x = np.empty((len(chars), ONE_HOT_SIZE), dtype=np.float)
     y = np.empty((len(chars) - 1,), dtype=np.float)
     for j, char in enumerate(chars):
