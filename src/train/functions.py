56d3853dc05024bc5eb2b2844d592b242a0f1d41
BryanCai <bryan.x.cai@gmail.com>
2017-05-12 06:06:27 -0400

    added distinguisher network

--- src/train/functions.py
+++ src/train/functions.py
@@ -7,25 +7,6 @@ def resize_matrix(matrix):
     output[-(i + 1)] = matrix[-(i + 1)]
   return output
 
-def password_to_data(filename):
-  X = np.load(filename)['X']
-  Y = np.load(filename)['Y']
-  outx_1 = []
-  outx_2 = []
-  outy = []
-  for i in range(len(X)):
-    if len(X[i]) == 0:
-      continue
-    forward_word = X[i]
-    reverse_word = X[i][::-1]
-    word_length = len(X[i])
-    timing = Y[i]
-    for j in range(len(forward_word) - 1):
-      outx_1.append(resize_matrix(forward_word[:j + 2]))
-      outx_2.append(resize_matrix(reverse_word[:word_length - j]))
-      outy.append(min(timing[j], max(np.random.normal(loc=150,scale=10), 0)))
-  return [np.array(outx_1), np.array(outx_2), np.array(outy)]
-
 def wordmatrix_to_input(wordmatrix):
   forward_word = np.array(wordmatrix)
   reverse_word = forward_word[::-1]
@@ -37,13 +18,53 @@ def wordmatrix_to_input(wordmatrix):
     outx_2.append(resize_matrix(reverse_word[:word_length - j]))
   return [np.array(outx_1), np.array(outx_2)]
 
-def filelist_to_data(filelist):
+def password_to_mimic_data(filename):
+  X = np.load(filename)['X']
+  Y = np.load(filename)['Y']
+  outx_1 = []
+  outx_2 = []
+  outy = []
+  for i in range(len(X)):
+    if len(Y[i]) == 0:
+      continue
+    forward_word = X[i]
+    reverse_word = X[i][::-1]
+    word_length = len(X[i])
+    timing = Y[i]
+    for j in range(len(forward_word) - 1):
+      outx_1.append(resize_matrix(forward_word[:j + 2]))
+      outx_2.append(resize_matrix(reverse_word[:word_length - j]))
+      outy.append(min(timing[j], max(np.random.normal(loc=150,scale=10), 0)))
+  return [np.array(outx_1), np.array(outx_2), np.array(outy)]
+
+def filelist_to_mimic_data(filelist):
   x_1 = np.zeros((0, c.WINDOW_SIZE, c.ALPHABET_SIZE))
   x_2 = np.zeros((0, c.WINDOW_SIZE, c.ALPHABET_SIZE))
   y = np.zeros(0)
   for file in filelist:
-    out = password_to_data(file)
+    out = password_to_mimic_data(file)
     x_1 = np.append(x_1, out[0], axis = 0)
     x_2 = np.append(x_2, out[1], axis = 0)
     y = np.append(y, out[2])
   return [x_1, x_2, y]
+
+def password_to_distinguish_data(filename):
+  X = np.load(filename)['X']
+  Y = np.load(filename)['Y']
+  output = []
+  for i in range(len(X)):
+    if len(Y[i]) == 0:
+      continue
+    a = np.zeros((c.MAX_WORD_SIZE, c.ALPHABET_SIZE + 1))
+    a[c.MAX_WORD_SIZE - len(X[i])] = np.append(X[i][0], 0)
+    for j in range(len(Y[i])):
+      a[c.MAX_WORD_SIZE - len(X[i]) + j + 1] = np.append(X[i][j + 1], Y[i][j]*0.002)
+    output.append(a)
+  return output
+
+def filelist_to_distinguish_data(filelist):
+  x = np.zeros((0, c.MAX_WORD_SIZE, c.ALPHABET_SIZE + 1))
+  for file in filelist:
+    out = password_to_distinguish_data(file)
+    x = np.append(x, out, axis = 0)
+  return x
--- src/train/train.py
+++ src/train/train.py
@@ -1,11 +1,13 @@
+import os
 import sys
 import glob
 import constants as c
 import functions as f
+import numpy as np
 
 from keras.models import Sequential, Model
 from keras.layers import SimpleRNN
-from keras.layers import Activation, Dropout, Flatten, Dense, Concatenate, Masking
+from keras.layers import Activation, Dropout, Flatten, Dense, Concatenate, Masking, LeakyReLU
 from keras.layers import Input
 from keras.optimizers import Nadam, SGD
 from keras.callbacks import Callback
@@ -17,11 +19,15 @@ def train_mimic(username):
   forward_input = Input(shape = (c.WINDOW_SIZE, c.ALPHABET_SIZE))
   forward_mask = Masking()(forward_input)
   forward_rnn = SimpleRNN(c.VECTOR_SIZE)(forward_mask)
+  forward_rnn = LeakyReLU()(forward_rnn)
   reverse_input = Input(shape = (c.WINDOW_SIZE, c.ALPHABET_SIZE))
   reverse_mask = Masking()(reverse_input)
   reverse_rnn = SimpleRNN(c.VECTOR_SIZE)(reverse_mask)
+  reverse_rnn = LeakyReLU()(reverse_rnn)
   x = Concatenate()([forward_rnn, reverse_rnn])
+  x = Dropout(0.5)(x)
   output = Dense(50)(x)
+  output = LeakyReLU()(output)
   output = Dense(1)(output)
 
   model = Model(inputs=[forward_input, reverse_input], outputs=output)
@@ -32,7 +38,7 @@ def train_mimic(username):
   model.compile(loss='mean_absolute_error', optimizer='sgd')
 
   input_files = glob.glob('Data/' + username + '/*.npz')
-  out = f.filelist_to_data(input_files)
+  out = f.filelist_to_mimic_data(input_files)
   model.fit(x=out[:2], y=out[2], epochs=c.EPOCHS)
 
   print "Saving result..."
@@ -40,29 +46,38 @@ def train_mimic(username):
 
 def train_distinguish(username):
   print "Creating Model..."
-  forward_input = Input(shape = (c.WINDOW_SIZE, c.ALPHABET_SIZE))
-  forward_mask = Masking()(forward_input)
-  forward_rnn = SimpleRNN(c.VECTOR_SIZE)(forward_mask)
-  reverse_input = Input(shape = (c.WINDOW_SIZE, c.ALPHABET_SIZE))
-  reverse_mask = Masking()(reverse_input)
-  reverse_rnn = SimpleRNN(c.VECTOR_SIZE)(reverse_mask)
-  x = Concatenate()([forward_rnn, reverse_rnn])
-  output = Dense(50)(x)
-  output = Dense(1)(output)
+  rnn_input = Input(shape = (c.MAX_WORD_SIZE, c.ALPHABET_SIZE + 1))
+  rnn_mask = Masking()(rnn_input)
+  rnn = SimpleRNN(c.VECTOR_SIZE)(rnn_mask)
+  rnn = LeakyReLU()(rnn)
+  rnn = Dropout(0.5)(rnn)
+  output = Dense(50)(rnn)
+  output = LeakyReLU()(output)
+  output = Dense(2, activation='softmax')(output)
 
-  model = Model(inputs=[forward_input, reverse_input], outputs=output)
+  model = Model(inputs=rnn_input, outputs=output)
 
   print "Compiling Model..."
   # optimizer = SGD(lr=0.05, momentum=0.0, decay=5e-4, nesterov=False)
   # optimizer = SGD(lr=0.01, momentum=0.0, decay=0.0, nesterov=False)
-  model.compile(loss='mean_absolute_error', optimizer='sgd')
+  model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
-  input_files = glob.glob('Data/' + username + '/*.npz')
-  out = f.filelist_to_data(input_files)
-  model.fit(x=out[:2], y=out[2], epochs=c.EPOCHS)
+  names = next(os.walk('./Data/'))[1]
+  x_data = np.zeros((0, c.MAX_WORD_SIZE, c.ALPHABET_SIZE + 1))
+  y_data = np.zeros((0, 2))
+  for name in names:
+    input_files = glob.glob('Data/' + name + '/*.npz')
+    x = f.filelist_to_distinguish_data(input_files)
+    if name == username:
+      y = np.repeat(np.array([[1, 0]]), len(x), axis=0)
+    else:
+      y = np.repeat(np.array([[0, 1]]), len(x), axis=0)
+    x_data = np.append(x_data, x, axis = 0)
+    y_data = np.append(y_data, y, axis = 0)
+  model.fit(x=x_data, y=y_data, epochs=c.EPOCHS)
 
   print "Saving result..."
-  model.save('models/' + username + '-mimic.h5')
+  model.save('models/' + username + '-distinguish.h5')
 
 def main(mode, username):
   if mode == 'mimic':
