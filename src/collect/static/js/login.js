95274f57c66b92fd27fd1c017d0292acf82e794c
Yasyf Mohamedali <yasyfm@gmail.com>
2017-05-14 23:53:02 -0400

    Add more feedback when submitting.

--- src/collect/static/js/login.js
+++ src/collect/static/js/login.js
@@ -1,4 +1,5 @@
 function evaluate(password) {
+  $('#response').html('Submitting...');
   let username = $('#username').val();
   post('/evaluate', {username, password}).then(resp =>
     $('#response').html(`[${resp.likelihood}] ${resp.success ? 'Success!' : 'Failed :('}`)
